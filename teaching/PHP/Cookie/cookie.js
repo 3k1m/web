d51608e55576f9ed5f887df5fc4decb1e18ca023
3k1m <39969119+3k1m@users.noreply.github.com>
2019-02-25 07:54:41 -0800

    Update cookie.js

--- teaching/PHP/Cookie/cookie.js
+++ teaching/PHP/Cookie/cookie.js
@@ -3,6 +3,6 @@ function add(){
   let type = "cat";
   let expiry = new Date();
   const day = 24*3600*1000;
-  expiry = expiry.setTime(expiry.getTime() + day);
+  expiry.setTime(expiry.getTime() + day);
   document.cookie = name + "=" + type + "; expires=" + expiry.toUTCString() + "; path=/";
 }
